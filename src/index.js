6a571ae544ea2b875374fbf7810b33bf889d3b58
Zolotou <vadimkarulin@gmail.com>
2020-03-20 02:58:22 +0300

    feat: make solutions to typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  
+  let sort = array.sort(function(a,b){return a-b});
+  return sort[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  if (array.length == 0){
+    return 0;
+  }
+  let sort = array.sort(function(a,b){return a-b});
+  return sort[sort.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  let average = array.reduce((acc, item) => acc + item);
+  return average/array.length;
 }
